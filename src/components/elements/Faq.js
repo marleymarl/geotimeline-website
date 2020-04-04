568972c5ce39926a776d610a788a3c11325e8205
Woosik Koong <woosik.koong@gmail.com>
2020-04-04 16:34:41 -0700

    Refactor components

--- src/components/elements/Faq.js
+++ src/components/elements/Faq.js
@@ -1,42 +1,9 @@
 import React from 'react';
 import { Collapse, Typography } from 'antd';
-//
+import { FAQs as data } from '../../utils/StringUtils';
+// imports
 const { Title, Paragraph } = Typography;
 const { Panel } = Collapse;
-
-const data = [
-  {
-    question: 'What does GeoTimeline.com do?',
-    answer:
-      'GeoTime is a data set of crowd sourced contact tracing information that can help the general public better understand the spread of COVID19 in your area.As people voluntarily post their locations, Geotime anonymously collects information instantly and provides a live version of contact tracing around the world. ',
-  },
-  {
-    question: 'What is contact tracing ?',
-    answer:
-      'Contact tracing is the process of identifying persons who may have come into contact with an infected person("contacts") and subsequent collection of further information about these contacts.This procedure is an effective public health practice to contain infectious diseases, such as measles, influenza, and Ebola. (According to WHO)',
-  },
-  {
-    question:
-      'Is it mandatory to use the application if I have tested positive ?',
-    answer:
-      'No, the website is anonymous and voluntary.However, please make sure you have access to an internet browser.',
-  },
-  {
-    question: 'How does Geotimeline work ?',
-    answer:
-      'In broad strokes, what we are trying to do is provide a safe and anonymous way for patients with confirmed cases of COVID 19 to voluntarily share their movements with public health officials and the general public.The accumulation of these data can help medical agencies trace hotspots of the virus.Members of the community can also use the data to make transportation decisions.Our goal is to help with the contact tracing efforts of public health agencies, without losing anonymity for people who test positive. ',
-  },
-  {
-    question: 'How much does it cost ?',
-    answer:
-      'Geotime is free to the general public and free to the developers who want to use our data set. ',
-  },
-  {
-    question: 'What happens to my data once I give permission for its use ?',
-    answer:
-      'The only permission so far, is the location access for your browser.We use it to take the user to his / her current location.Nothing happens after that, but you must explicitly give the application permission to access your location data.',
-  },
-];
 
 const Faq = props => {
   function callback(key) {
