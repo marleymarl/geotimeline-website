16ec37df7e3840b88ef9fb6db8e3944b3b27bd9f
BaN <fabanaza@gmail.com>
2020-04-18 14:25:58 +0430

    modify texts in slides

--- src/utils/StringUtils.js
+++ src/utils/StringUtils.js
@@ -64,8 +64,8 @@ export const CarouselData = [
 ];
 
 export const photoCollection = [
-  ['CC','For Confirmed Case', ['CC1.png', 'CC2.png', 'CC3.png', 'CC4.png', 'CC5.png', 'CC6.png']],
-  ['HE','For Health Expert', ['HE1.png', 'HE2.png', 'HE3.png', 'HE4.png', 'HE5.png', 'HE6.png']],
-  ['DS','For Data Scientist', ['DS1.png', 'DS2.png', 'DS3.png', 'DS4.png', 'DS5.png', 'DS6.png']],
-  ['A','For Anyone', ['A1.png', 'A2.png', 'A3.png', 'A4.png', 'A5.png', 'A6.png']]
+  ['CC','For people who test positive', ['CC1.png', 'CC2.png', 'CC3.png', 'CC4.png', 'CC5.png', 'CC6.png']],
+  ['HE','For contact tracer', ['HE1.png', 'HE2.png', 'HE3.png', 'HE4.png', 'HE5.png', 'HE6.png', 'HE7.png']],
+  ['DS','For data scientist', ['DS1.png', 'DS2.png', 'DS3.png', 'DS4.png', 'DS5.png', 'DS6.png']],
+  ['A','For everyone else', ['A1.png', 'A2.png', 'A3.png', 'A4.png', 'A5.png', 'A6.png']]
 ]
